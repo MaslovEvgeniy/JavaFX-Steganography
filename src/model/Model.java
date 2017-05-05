9d7c8d7943de4433003b69b428ce7c65242e99f6
AnnNemch <ann_nemchenko@mail.ru>
2017-05-05 14:08:22 +0300

    Algorithm

--- src/model/Model.java
+++ src/model/Model.java
@@ -15,7 +15,7 @@ public class Model {
     }
 
     public Image encode(Image image, String message) {
-        return encoder.encode(image, message, 6);
+        return encoder.encode(image, message, 1);
     }
 
     //decoder
