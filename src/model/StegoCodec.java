bb4c2a2d901fdfb631419ed98b5c41d4ffdc4cbc
Evgeniy Maslov <maslovevgeniy98@gmail.com>
2017-05-15 13:22:25 +0300

    Upd

--- src/model/StegoCodec.java
+++ src/model/StegoCodec.java
@@ -12,6 +12,10 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+
+/**
+ * Class for hiding information using LSB-method
+ */
 public class StegoCodec {
 
     private BufferedImage currentImage;
@@ -19,6 +23,14 @@ public class StegoCodec {
     private int currentBit = 7;
     private int currentByte = 0;
 
+    /**
+     * Encode secret text message into image
+     *
+     * @param image   image, where text will be hidden
+     * @param message text to hide
+     * @return image with hidden text
+     * @throws ArrayIndexOutOfBoundsException if text size is more than the amount information that image can contain
+     */
     public Image encodeText(Image image, String message) throws ArrayIndexOutOfBoundsException {
         this.currentImage = SwingFXUtils.fromFXImage(image, null);
         byte[] msgBytes = null;
@@ -33,6 +45,14 @@ public class StegoCodec {
         return encode(msgBytes);
     }
 
+    /**
+     * Encode image to image
+     *
+     * @param image        image, where another image will be hidden
+     * @param hidImagePath path to image that will be hidden
+     * @return image with hidden information
+     * @throws ArrayIndexOutOfBoundsException if image can not contain
+     */
     public Image encodeImage(Image image, String hidImagePath) throws ArrayIndexOutOfBoundsException {
         this.currentImage = SwingFXUtils.fromFXImage(image, null);
         byte[] imgBytes = null;
@@ -47,38 +67,64 @@ public class StegoCodec {
         return encode(imgBytes);
     }
 
+    /**
+     * Decode hidden text
+     *
+     * @param image image with hidden text
+     * @return extracted text
+     * @throws IOException if image doesn't contain hidden text
+     */
     public String decodeText(Image image) throws IOException {
         this.currentImage = SwingFXUtils.fromFXImage(image, null);
-        if(!decode())
+        if (!decode())
             throw new IOException();
-        String result = new String(data);
+        String result = new String(data, "UTF-8");
         data = null;
         this.currentByte = 0;
         this.currentBit = 7;
         return result;
     }
 
+    /**
+     * Decode hidden image
+     *
+     * @param image image with encoded information
+     * @return decoded image
+     * @throws IOException if image doesn't contain hidden information
+     */
     public Image decodeImage(Image image) throws IOException {
         this.currentImage = SwingFXUtils.fromFXImage(image, null);
-        if(!decode())
-            throw new IOException();;
+        if (!decode())
+            throw new IOException();
+        ;
         BufferedImage img = null;
         try {
             img = ImageIO.read(new ByteArrayInputStream(data));
+            if (img == null) throw new IOException();
         } catch (IOException e) {
-            e.printStackTrace();
+            throw e;
         }
+
         Image imageRes = SwingFXUtils.toFXImage(img, null);
         return imageRes;
     }
 
+    /**
+     * Encode information bytes into current image
+     *
+     * @param msg bytes of information that must be hidden
+     * @return image with hidden information
+     */
     private Image encode(byte[] msg) {
         byte[] data = new byte[4 + msg.length];
         byte[] msgLength = intToByteArray(msg.length);
+
+        //merge two arrays into one
         System.arraycopy(msgLength, 0, data, 0, msgLength.length);
         System.arraycopy(msg, 0, data, 4, msg.length);
         this.data = data;
 
+        //get current image width and height
         int w = currentImage.getWidth();
         int h = currentImage.getHeight();
 
@@ -89,6 +135,7 @@ public class StegoCodec {
         int blue = c.getBlue() & 0xFE;
         currentImage.setRGB(0, 0, new Color(red, green, blue, c.getAlpha()).getRGB());
 
+        //inserting information bytes into image
         int i = 0, r = 0, g = 0, b = 0;
         Color color = null;
         try {
@@ -100,18 +147,25 @@ public class StegoCodec {
                 currentImage.setRGB(i % w, i / w, new Color(r, g, b, color.getAlpha()).getRGB());
             }
         } catch (ArrayIndexOutOfBoundsException e) {
+            //return to last byte of message and extract last bit, that don't inserted into image
+            //by the reason of catching exception before it
             currentByte--;
-            currentBit = 7;
+            currentBit = 6;
             g = (color.getGreen() & 0xFE | (generateNextBit()));
-            b = color.getBlue();
+            b = (color.getBlue() & 0xFE | (generateNextBit()));
             currentImage.setRGB(i % w, i / w, new Color(r, g, b, color.getAlpha()).getRGB());
 
-            Image image = SwingFXUtils.toFXImage(currentImage, null);
+            Image image = SwingFXUtils.toFXImage(currentImage, null);//convert BufferedImage into Image
             return image;
         }
         return null;
     }
 
+    /**
+     * Decode hidden information into byte array
+     *
+     * @return true - if information successfully decoded, false - if image doesn't contain hidden information
+     */
     private boolean decode() {
         this.data = null;
         this.currentByte = 0;
@@ -122,21 +176,23 @@ public class StegoCodec {
 
         Color c = new Color(currentImage.getRGB(0, 0));
         int lsb = 0;
-        lsb = (c.getBlue() & 1) + ((c.getGreen() & 1) << 1) + ((c.getRed() & 1) << 2);
+        lsb = (c.getBlue() & 1) + ((c.getGreen() & 1) << 1) + ((c.getRed() & 1) << 2);//get hidden information from 1-st pixel
 
-
+        //decode length of hidden information
         int length = 0;
         this.data = new byte[4];
         int i = 1;
         try {
             for (i = 1; i < w * h; i++) {
+                //extract pixel from image and set his last bits into data array
                 Color color = new Color(currentImage.getRGB(i % w, i / w));
                 setNextBit(color.getRed() & 1);
                 setNextBit(color.getGreen() & 1);
                 setNextBit(color.getBlue() & 1);
             }
-        } catch (ArrayIndexOutOfBoundsException e) {
+        } catch (ArrayIndexOutOfBoundsException e) {//if we extracted needed 4 bytes
             length = ByteBuffer.wrap(data).getInt();
+            this.data = null;
             this.data = new byte[length];
             this.currentByte = 0;
             this.currentBit = 7;
@@ -146,34 +202,53 @@ public class StegoCodec {
         if (lsb != 0 || (length <= 0 || length > Integer.MAX_VALUE - 4)) //check if we have encoded message
             return false;
 
+        Color color = null;
         try {
             for (i = 12; i < w * h; i++) {
-                Color color = new Color(currentImage.getRGB(i % w, i / w));
+                color = new Color(currentImage.getRGB(i % w, i / w));
                 setNextBit(color.getRed() & 1);
                 setNextBit(color.getGreen() & 1);
                 setNextBit(color.getBlue() & 1);
             }
-        } catch (ArrayIndexOutOfBoundsException e) {
+        } catch (ArrayIndexOutOfBoundsException e) {//if whole message have decoded
             return true;
         }
         return false;
     }
 
+    /**
+     * Checks if we can hide needed information into current image
+     *
+     * @param msgLength amount of information bytes
+     * @return true - if information can be hidden, false - otherwise
+     */
     private boolean checkSize(int msgLength) {
         return (msgLength + 4) * 8 + 3 < currentImage.getWidth() * currentImage.getHeight() * 3;
     }
 
-    private byte[] intToByteArray(int length) {
+    /**
+     * Converts int number into array of 4 bytes
+     *
+     * @param number int number that must be converted
+     * @return byte array with converted number
+     */
+    private byte[] intToByteArray(int number) {
         byte[] result = new byte[4];
 
-        result[0] = (byte) ((length & 0xFF000000) >> 24);
-        result[1] = (byte) ((length & 0x00FF0000) >> 16);
-        result[2] = (byte) ((length & 0x0000FF00) >> 8);
-        result[3] = (byte) ((length & 0x000000FF));
+        result[0] = (byte) ((number & 0xFF000000) >> 24);
+        result[1] = (byte) ((number & 0x00FF0000) >> 16);
+        result[2] = (byte) ((number & 0x0000FF00) >> 8);
+        result[3] = (byte) ((number & 0x000000FF));
 
         return result;
     }
 
+    /**
+     * Generate next bit from data byte array
+     *
+     * @return 1 or 0, depending on which value contains in data
+     * @throws ArrayIndexOutOfBoundsException if we used all data bytes
+     */
     private int generateNextBit() throws ArrayIndexOutOfBoundsException {
         int r = (data[currentByte] >> currentBit) & 1;
         if (--currentBit < 0) {
@@ -183,6 +258,12 @@ public class StegoCodec {
         return r;
     }
 
+    /**
+     * Sets next bit into data byte array
+     *
+     * @param value bit to be written into array
+     * @throws ArrayIndexOutOfBoundsException if exceeded needed size for array
+     */
     private void setNextBit(int value) throws ArrayIndexOutOfBoundsException {
         data[currentByte] = (byte) (data[currentByte] | value << currentBit);
         if (--currentBit < 0) {
@@ -190,6 +271,5 @@ public class StegoCodec {
             currentBit = 7;
         }
     }
-
 
 }
